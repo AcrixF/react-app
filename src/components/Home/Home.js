6f3441824a54c299444aeae4f91ba2061d80e277
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-05-18 13:32:21 -0500

    Local State Update / componentDidMount

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,28 +3,21 @@ import './Home.css';
 
 export default class Home extends Component {
 
-    render() {
-
-        const buttonStyle = {
-            backgroundColor: 'gray',
-            border: '1px solid black'
+    constructor() {
+        super();
+        this.state = {
+            name: 'Cristian'
         }
-
-        return(
-        <div className="Home">
-            <h1>Welcome to CodeJobs</h1>
-            <p>
-                In this recipe you will learn how to add styles to
-                components. If you want to learn more you can visit
-                our Youtube Channel at
-                <a href="http://youtube.com/codejobs"> Codejobs</a>.
-            </p>
-            <p>
-                <button style={buttonStyle}>
-                    Click Me
-                </button>
-            </p>
-        </div>)
     }
+
+    componentDidMount = () => {
+        setTimeout(() => { this.setState({name:'Cristina'})}, 2000)
+    }
+
+
+    render = () =>
+        <div className="Home">
+            <p>Hi my name is {this.state.name}</p>
+        </div>
 
 }
